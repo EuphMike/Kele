86267cec44a1bfe75f4e512d4be5b1a98d75bbb5
Mike Waddell <waddellmusic@gmail.com>
2017-09-04 20:53:54 +0000

    completedCheckpoint6

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,10 +1,10 @@
 module Roadmap 
-    def get_roadmap
+    def get_roadmap(roadmap_id)
         response = self.class.get(api_url("roadmaps/31"), headers: { "authorization" => @auth_token })
         @roadmap_data = JSON.parse(response.body)
     end
     
-    def get_checkpoint
+    def get_checkpoint(checkpoint_id)
         response = self.class.get(api_url("checkpoints/id"), headers: { "authorization" => @auth_token })
         @checkpoint_data = JSON.parse(response.body)
     end
