ae711b76d06b7140f7eaacff848d83e4116a82f9
Mike Waddell <waddellmusic@gmail.com>
2017-09-04 22:16:03 +0000

    completedCheckpoint7

--- lib/kele.rb
+++ lib/kele.rb
@@ -23,6 +23,18 @@ class Kele
     response = self.class.get(api_url("mentors/id/student_availability"), headers: { "authorization" => @auth_token })
     @mentor_availability = JSON.parse(response.body)
   end
+  
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    response = self.class.post(api_url("checkpoint_submissions"),
+    body: {
+            "checkpoint_id": checkpoint_id,
+            "assignment_branch": assignment_branch,
+            "assignment_commit_link": assignment_commit_link,
+            "comment": comment,
+            },
+    headers: {"authorization" => @auth_token})
+    puts response
+  end
 
   private
   def api_url(endpoint)
